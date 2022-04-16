0398dea03fe71bfb8f94bbea0f1555d8b7fe9b55
Vladislav Pestsov <pestsov.js@gmail.com>
2022-04-16 14:24:04 +0300

    review orders code

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -1,9 +1,27 @@
 const BaseController = require("../../core/base/base.controller");
 const Order = require("./Order.model");
+const OrderHelper = require("./order.helper");
+const OrderService = require("./order.service");
 
 class OrderController {
   getOrder = BaseController.getOne(Order);
   deleteOrder = BaseController.deleteOne(Order);
+
+  async getOrders(req, res) {
+    const orders = await OrderService.getOrder(req.user._id);
+    const order = OrderHelper.getOrders(orders);
+
+    OrderHelper.responseObject(order, 200, res);
+  }
+
+  async createOrder(req, res) {
+    const user = await OrderService.getBasket(req.session.user._id);
+    const products = OrderHelper.getProducts(user);
+    const order = OrderService.newOrder(user, products);
+
+    OrderHelper.sendOrder(order, req.user);
+    OrderHelper.responseObject(order, 200, res);
+  }
 }
 
 module.exports = new OrderController();
--- src/modules/order/order.router.js
+++ src/modules/order/order.router.js
@@ -1,65 +1,11 @@
 const express = require("express");
-const User = require("../user/User.model");
 const OrderController = require("./order.controller");
-const OrderHelper = require("./order.helper");
-const Order = require("./Order.model");
 const orderPath = require("./order.router.path");
 const orderRouter = express.Router();
 
-// add CRUD order
-
+orderRouter.get(orderPath.orders, OrderController.getOrders);
 orderRouter.get(orderPath.order, OrderController.getOrder);
+orderRouter.post(orderPath.orders, OrderController.createOrder);
 orderRouter.post(orderPath.order, OrderController.deleteOrder);
-
-orderRouter.get(orderPath.orders, async (req, res) => {
-  const orders = await Order.find({ "user.userId": req.user._id }).populate(
-    "user.userId"
-  );
-
-  const order = orders.map((o) => {
-    return {
-      ...o._doc,
-      price: o.products.reduce((total, p) => {
-        return (total += p.count * p.product.price);
-      }, 0),
-    };
-  });
-
-  res.status(200).json({
-    status: "message",
-    data: {
-      order: order,
-    },
-  });
-});
-
-orderRouter.post(orderPath.orders, async (req, res) => {
-  const user = await User.findById(req.session.user._id).populate(
-    "basket.items.productId"
-  );
-
-  const products = user.basket.items.map((i) => ({
-    count: i.count,
-    product: { ...i.productId._doc },
-  }));
-
-  const order = new Order({
-    user: {
-      firstName: user.firstName,
-      lastName: user.lastName,
-      userId: req.user,
-    },
-    products: products,
-  });
-
-  await order.save();
-  await req.user.clearBasket();
-  res.status(200).json({
-    status: "success",
-    data: {
-      order: order,
-    },
-  });
-});
 
 module.exports = orderRouter;
