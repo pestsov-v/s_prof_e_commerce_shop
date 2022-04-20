082c591570a5801810c400b6bcf7d86c06a0c29c
Vladislav Pestsov <pestsov.js@gmail.com>
2022-04-20 22:09:48 +0300

    add the login documentation in swagger

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -8,7 +8,6 @@ const authRouter = express.Router();
 authRouter.post(authPath.signup, AuthController.signup);
 authRouter.post(authPath.login, AuthController.login);
 authRouter.post(authPath.logout, AuthMiddleware.protect, AuthController.logout);
-
 authRouter.post(authPath.password, AuthController.forgotPassword);
 authRouter.patch(authPath.reset, AuthController.resetPassword);
 
